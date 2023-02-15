097a7396a806359904717e00516b572e446e4215
onePoint <124888518+Gvozdkov@users.noreply.github.com>
2023-02-15 15:48:47 +0300

    Initial Commit

---
+++ sprint3.homework.Gvozdkov/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sprint3.homework.Gvozdkov
+//
+//  Created by Алексей Гвоздков on 15.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
