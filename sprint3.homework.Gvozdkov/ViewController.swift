ab1478871a4f0e4201100c546480b973b5b7610c
onePoint <124888518+Gvozdkov@users.noreply.github.com>
2023-02-15 16:02:18 +0300

    Создано приложение счетчик. При нажатие на кнопку label выводит сколько раз было нажато на кнопку.

--- sprint3.homework.Gvozdkov/ViewController.swift
+++ sprint3.homework.Gvozdkov/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttonCounter: UIButton!
+    @IBOutlet var labelCounter: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonCounter.tintColor = .orange
     }
 
+    private var counter: Int = 0
+    @IBAction func buttonCounterFunc(_ sender: Any) {
+        counter += 1
+                labelCounter.text = "Значение счётчика: \(counter)"
 
+    }
+    
 }
 
